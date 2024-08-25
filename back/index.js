dfd9fcb318780c58c060488a875d56fe30970fa8
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-08-25 16:35:18 +0530

    Update index.js

--- back/index.js
+++ back/index.js
@@ -3,23 +3,36 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
-const cors=require('cors')
+const cors = require('cors');
+
 dotenv.config();
 
 const app = express();
 
+// CORS Configuration
+const allowedOrigins = ['https://management-system-rjpu.vercel.app']; // Add the allowed origins here
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
-app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('Failed to connect to MongoDB', err));
-
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
